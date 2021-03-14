decca102686f2c79cc6f7a4093105e39715ed1bc
Tri Phan <phanphutri@gmail.com>
2021-03-14 09:00:49 +0700

    Updated main.js

--- chap02/js/main.js
+++ chap02/js/main.js
@@ -1,2 +1,20 @@
-let number = Number(prompt('Pick a number'));
-console.log(`The number is the square root of ${number * number}`);
+// let number = Number(prompt('Pick a number'));
+// console.log(`The number is the square root of ${number * number}`);
+
+// CONDITIONAL EXECUTION
+// let num = Number(prompt('Pick a number'));
+// if (!Number.isNaN(num)) {
+//     console.log(`The number is the square root of ${num * num}`);
+// } else {
+//     console.log("Hey. Why didn't you give me a number?");
+// }
+
+// Chain multiple if/else pairs together
+let num = Number(prompt('Pick a number'));
+if (num < 10) {
+    console.log('Small');
+} else if (num < 100) {
+    console.log('Medium');
+} else {
+    console.log('Large');
+}

3f510a21695708a80915c6295a5a59072000fc80
Tri Phan <phanphutri@gmail.com>
2021-03-14 11:44:07 +0700

    Created chap02/exercise.html, chap02/js/exercises.js

---
+++ chap02/js/exercises.js
@@ -0,0 +1,44 @@
+/*
+  Exercise 01
+  LOOPING A TRIANGLE
+  Write a loop that makes seven calls to console.log to output the following
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+  
+  It's maybe useful to know that you can find the length of a string by 
+  writing .length after it
+
+  Example:
+  let abc = 'abc';
+  console.log(abc.length); 
+  // -> 3
+*/
+
+// SOLUTION
+// let str = '#######';
+// for (let index = 0; index < str.length; index++) {
+//   console.log(str.slice(0, index + 1));
+// }
+
+// function printTriangle() {
+//   let str = '#######';
+//   for (let index = 0; index < str.length; index++) {
+//     console.log(str.slice(0, index + 1));
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', printTriangle); // DOM ready function
+// -------------------------------------------------------------------------------------------
+
+/*
+  Exercise 02
+  FIZZBUZZ
+  Write a program 
+  
+
+*/

fcd6f70259782c58a90e90f2de5a52a951c886ab
Tri Phan <phanphutri@gmail.com>
2021-03-15 15:30:51 +0700

    Updated chap03_Functions/js/main.js

--- chap03_Functions/js/main.js
+++ chap03_Functions/js/main.js
@@ -192,4 +192,79 @@
 // console.log(square(4, 2));
 // console.log(square(4));
 
-console.log('a', 'b', 122);
+// console.log('a', 'b', 122);
+// ---------------------------------------------------------------------------
+
+// CLOSURE
+// function wrapValue(n) {
+//     let local = n;
+//     return () => local;
+// }
+
+// let wrap1 = wrapValue(1);
+// let wrap2 = wrapValue(2);
+
+// console.log(wrap1());
+// console.log(wrap2());
+
+// Another example:
+// function multiplier(factor) {
+//     return (number) => number * factor;
+// }
+
+// let twice = multiplier(2);
+// // console.log(twice(5));
+// console.log(twice(10));
+// ---------------------------------------------------------------------------
+
+// CLOSURE (FROM MDN)
+// link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
+/**
+ * A closure is the combination of a function bundled together (enclosed)
+ * with references to its surrounding state (the lexical environment).
+ * In other words, a closure gives you access to an outer function's scope
+ * from an inner function.
+ * In Javascript, closures are created everytime a function is created,
+ * at function creation time.
+ */
+
+// 1. Lexical scoping
+// Example:
+// (Note: Nested function have access to variables declared in their outer scope)
+// function init() {
+//     let name = 'Mozilla'; // name is a local variable created by init
+//     // displayName() is the inner function,
+//     // a closure use variable declared in the parent function
+//     function displayName() {
+//         console.log(name);
+//     }
+//     displayName();
+// }
+// init();
+
+// 2. Closure
+// Example 1:
+// function makeFunc() {
+//     let name = 'Mozilla';
+//     function displayName() {
+//         console.log(name);
+//     }
+//     return displayName;
+// }
+
+// let myFunc = makeFunc();
+// myFunc();
+
+// Example 2:
+function makeAdder(x) {
+    return function (y) {
+        return x + y;
+    };
+}
+
+let add5 = makeAdder(5);
+let add10 = makeAdder(10);
+
+console.log(add5(2)); // -> 7
+console.log(add10(2)); // -> 12

2f0b8d3652f35345f23f21308853319f4cbe2f8e
Tri Phan <phanphutri@gmail.com>
2021-03-15 13:53:01 +0700

    Updated chap03_Functions/index.html, chap03_Functions/js/main.js

--- chap03_Functions/js/main.js
+++ chap03_Functions/js/main.js
@@ -26,12 +26,112 @@
 // ---------------------------------------------------------------------------
 
 // BINDINGS AND SCOPES
-let x = 10;
-if (true) {
-  let y = 20;
-  var z = 30;
-  console.log(x + y + z); // ->60
-}
+// let x = 10;
+// if (true) {
+//   let y = 20;
+//   var z = 30;
+//   console.log(x + y + z); // ->60
+// }
 
 // y is not visible heare
-console.log(x + z); // -> 40
+// console.log(x + z); // -> 40
+
+// const halve = function (n) {
+//   return n / 2;
+// };
+
+// let n = 10;
+// console.log(halve(100));
+// console.log();
+
+// NESTED SCOPE
+// const hummus = function (factor) {
+//     const ingredient = function (amount, unit, name) {
+//         let ingredientAmout = amount * factor;
+//         if (ingredientAmout > 1) {
+//             unit += 's';
+//         }
+//         console.log(`${ingredientAmout} ${unit} ${name}`);
+//     };
+
+//     ingredient(1, 'can', 'chickpeas');
+//     ingredient(0.25, 'cup', 'tahini');
+//     ingredient(0.25, 'cup', 'lemon juice');
+//     ingredient(1, 'clove', 'garlic');
+//     ingredient(2, 'tablespoon', 'olive oil');
+//     ingredient(0.5, 'teaspoon', 'cumin');
+// };
+
+// hummus(1);
+// hummus(2);
+// ---------------------------------------------------------------------------
+
+// FUNCTIONS AS VALUES
+// let launchMissiles = function () {
+//     missileSystem.launch('now');
+// };
+
+// if (safeMode) {
+//     launchMissiles = function () {
+//         /* Do nothing */
+//     };
+// }
+// ---------------------------------------------------------------------------
+
+// DECLARATION NOTATION
+// function square(x) {
+//     return x * x;
+// }
+
+// let result = square(4);
+// console.log(result);
+
+// console.log('The future says: ', future());
+
+// function future() {
+//     return "You'll never flying a car.";
+// }
+// ---------------------------------------------------------------------------
+
+// ARROW FUNCTION
+
+// Normal function
+// const power = function (base, exponent) {
+//     let result = 1;
+//     for (let count = 0; count < exponent; count++) {
+//         result *= base;
+//     }
+//     return result;
+// };
+
+// Arrow function
+// const power = (base, exponent) => {
+//     let result = 1;
+//     for (let i = 0; i < exponent; i++) {
+//         result *= base;
+//     }
+//     return result;
+// };
+
+// console.log(power(4, 3));
+
+// When there is only one parameter name,
+// you can omit the parentheses around the parameter list
+// const square = (num) => {
+//     return num * num;
+// };
+
+// console.log(square(4));
+
+// When an arrow function has no parameters at all,
+// its parameter list is just an empty
+
+const power = (base, exponent) => {
+    let result = 1;
+    for (let i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+};
+
+console.log(power(4, 2));

f35f8147bbed6a66ffa5fdd6c9c46b8cc9bf1e8e
Tri Phan <phanphutri@gmail.com>
2021-03-16 10:30:11 +0700

    Updated chap03_Functions/js/exercises.js, added exercise 03

--- chap03_Functions/js/exercises.js
+++ chap03_Functions/js/exercises.js
@@ -83,3 +83,76 @@
 // console.log(isEven(75)); // false
 // console.log(isEven(-1)); // false
 // console.log(isEven(-14)); // true
+// --------------------------------------------------------------------------------------
+
+/**
+ * Exercise 03
+ * BEAN COUNTING
+    You can get the Nth character, or letter, from a string by writing "string"[N].
+    The returned value will be a string containing only one character (for example,
+    "b"). The first character has position 0, which causes the last one to be found at
+    position string.length - 1. In other words, a two-character string has length
+    2, and its characters have positions 0 and 1.
+
+    Write a function countBs that takes a string as its only argument and returns
+    a number that indicates how many uppercase “B” characters there are in the
+    string.
+    Next, write a function called countChar that behaves like countBs, except
+    it takes a second argument that indicates the character that is to be counted
+    (rather than counting only uppercase “B” characters). Rewrite countBs to
+    make use of this new function.
+ */
+
+// // Function
+// function countBs(string) {
+//     let count = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         const chr = string[i];
+//         if (chr === 'B') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// // Testing
+// console.log(countBs('BaaBnn')); // 2
+// console.log(countBs('BB3Bnn')); // 3
+// console.log(countBs('BBbBnn')); // 3
+// console.log(countBs('BBbBnnbbB')); // 4
+// -------------------------------------------
+
+// Function
+// function countChar(string, char) {
+//     let count = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         const chr = string[i];
+//         if (char === chr) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// // Testing
+// console.log(countChar('AAAa', 'a')); // 1
+// console.log(countChar('AaAAa', 'a')); // 2
+// console.log(countChar('aAaAAabbbb', 'a')); // 3
+// ------------------------------------------------------------
+
+// SOLUTION FROM WRITER
+// Function
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == ch) {
+            counted += 1;
+        }
+    }
+    return counted;
+}
+
+// Function
+function countBs(string) {
+    return countChar(string, 'B');
+}

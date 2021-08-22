4bcfd9ba7c4cc753a9b490e3f9d30276449f44a5
Tri Phan <phanphutri@gmail.com>
2021-08-22 08:32:51 +0700

    Updated

---
+++ chapter15_Handling_Events/Events/Pointer_Event_Mouse_Motion.js
@@ -0,0 +1,11 @@
+/** Pointer Event - Mouse Motion */
+
+/**
+ * Every time the mouse pointer moves, a "mousemove" event is fired. This event
+ * can be used to track the position of the mouse. A common situation in which
+ * this is useful is when implementing some form of mouse dragging functionality.
+ * 
+ * As an example, the following program displays a bar and sets up event handlers
+ * so that dragging to the left or right on this bar makes it narrower or wider:
+ */
+
